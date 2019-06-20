7234288c9ba90bc470fb4f3311fd6552e1a64511
MOHAMMAD PASHA <nag.chennamadhava123@gmail.com>
2019-06-20 18:25:42 +0530

    Initial commit

---
+++ src/Dell.java
@@ -0,0 +1,12 @@
+public class Dell {
+    public static void main(String[] args) {
+
+
+        int a, b, c;
+        a = 12;
+        b = 12;
+        c = a + b;
+        System.out.println("This is result of "+ c);
+
+    }
+}
